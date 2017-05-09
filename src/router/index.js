2ae4ecfc2f10dd321be71cd44734c1f218c42bee
Nurfirliana Muzanella <firli@flipbox.co.id>
2017-05-10 01:42:48 +0700

    [ADD] Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
+import routes from './routes'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Hello',
+//       component: Hello
+//     }
+//   ]
+// })
+
+const router = new Router({
+  routes
 })
+
+export default router
---
+++ src/router/routes/index.js
@@ -0,0 +1,55 @@
+const Dashboard = (resolve) => {
+  require.ensure(['@/pages/Dashboard'], () => {
+    resolve(require('@/pages/Dashboard'))
+  })
+}
+
+const Hello = (resolve) => {
+  require.ensure(['@/components/Hello'], () => {
+    resolve(require('@/components/Hello'))
+  })
+}
+
+const PageNotFound = (resolve) => {
+  require.ensure(['@/pages/error/404'], () => {
+    resolve(require('@/pages/error/404'))
+  })
+}
+
+const dashboard = {
+  path: '/',
+  name: 'dashboard',
+  component: Dashboard,
+  meta: {
+    breadcrumb: [{
+      to: {name: 'dashboard'},
+      label: 'Dashboard'
+    }]
+  }
+}
+
+const hello = {
+  path: '/hello',
+  name: 'hello',
+  component: Hello,
+  meta: {
+    breadcrumb: [{
+      to: {name: 'hello'},
+      label: 'Hello'
+    }]
+  }
+}
+
+export default [
+  dashboard,
+  hello,
+  {
+    name: 'pageNotFound',
+    path: '*',
+    component: PageNotFound,
+    meta: {
+      noLayout: true,
+      allowAnonymous: true
+    }
+  }
+]
